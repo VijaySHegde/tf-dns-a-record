ec92536512c43fb40bc939b97541b32d8d1dbee8
VijaySHegde <55663295+VijaySHegde@users.noreply.github.com>
2024-10-01 10:50:57 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ resource "walmart_dns_a_record" "eventstore_cluster_new_records" {
  # depends_on = var.custom_depends_on
   count      = var.virtual_machine_count
   ip_address = var.virtual_machine_nic_private_ip_address[count.index]
-  fqdn       = "${replace(var.es_cluster_name, "_", "-")}.var.domain_name"
+  fqdn       = "${replace(var.es_cluster_name, "_", "-")}.${var.domain_name}"
   ttl        = var.ttl
 }
 
@@ -10,6 +10,6 @@ resource "walmart_dns_a_record" "eventstore_cluster_new_nodes_records" {
   #depends_on = var.custom_depends_on
   count      = var.virtual_machine_count
   ip_address = var.virtual_machine_nic_private_ip_address[count.index]
-  fqdn       = "${replace(var.es_cluster_name, "_", "-")}-node${count.index+1}.var.domain_name"
+  fqdn       = "${replace(var.es_cluster_name, "_", "-")}-node${count.index+1}.${var.domain_name}"
   ttl        = var.ttl
 }
